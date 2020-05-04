4dc2697ac7f5a29ad5f64de6df6b74ddb3cd7c6c
gboned <gboned@cifpfbmoll.eu>
2020-05-04 23:46:17 +0200

    feat(Controlador): Añadir método para obtener el teléfono de la persona

--- src/main/java/org/formacio/mvc/Controlador.java
+++ src/main/java/org/formacio/mvc/Controlador.java
@@ -1,19 +1,31 @@
 package org.formacio.mvc;
 
 import org.formacio.repositori.AgendaService;
+import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Controlador extends AgendaService {
 	@Autowired
 	private AgendaService agendaService;
+	@Autowired
+	private Persona persona;
 	@RequestMapping(path = "/nombre")
 	@ResponseBody
 	public int numeroContactos() {
 		return agendaService.nombreContactes();
 	}
-	
+	@RequestMapping(path = "/telefon")
+	@ResponseBody
+	public String telefonoContactos(@RequestParam String id) {
+		return agendaService.devolverTelefono(id);
+	}
+		
+		
 }
+
+

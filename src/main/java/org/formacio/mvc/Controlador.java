8a752b0db2a4819818dfb6ef403c0d2b591817d6
gboned <gboned@cifpfbmoll.eu>
2020-05-05 23:27:12 +0200

    feat(Controlador): Crear método para devolver contacte y no nom

--- src/main/java/org/formacio/mvc/Controlador.java
+++ src/main/java/org/formacio/mvc/Controlador.java
@@ -1,20 +1,22 @@
 package org.formacio.mvc;
 
+import java.util.HashMap;
+
 import org.formacio.repositori.AgendaService;
-import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
+
 @Controller
 public class Controlador extends AgendaService {
 	@Autowired
 	private AgendaService agendaService;
-	@Autowired
-	private Persona persona;
 	@RequestMapping(path = "/nombre")
 	@ResponseBody
 	public int numeroContactos() {
@@ -29,11 +31,25 @@ public class Controlador extends AgendaService {
 			value = "/contacte/{id}", 
 			method = RequestMethod.GET, 
 			produces = "application/json")
+	
 	@ResponseBody
-	public Persona personaPorId() {
-		return persona;
-	}
+	public String personaPorId(@PathVariable("id") String id) {
+		String clau = id;
+		String nom = agendaService.devolverNombre(id);
+		String tel = agendaService.devolverTelefono(id);
 		
+		HashMap<String, String> persona = new HashMap<String, String>();
+		
+		persona.put("clau", clau);
+		persona.put("contacte", nom);
+		persona.put("telefon", tel);
+		
+		Gson parserPersona = new Gson();
+		String parsedPersona = parserPersona.toJson(persona);
+		
+		return parsedPersona;
+	}
+	
 }
 
 

f515f7a76be6b0bf262b8cc616b0d3d069837a96
gboned <gboned@cifpfbmoll.eu>
2020-05-07 16:57:10 +0200

    style & refactor(AgendaService): Añadir comentarios y eliminar método

--- src/main/java/org/formacio/repositori/AgendaService.java
+++ src/main/java/org/formacio/repositori/AgendaService.java
@@ -26,21 +26,32 @@ public class AgendaService extends Persona {
 	public Persona recupera (String id) {
 		return bbdd.get(id);
 	}
-	
+	/* Creado método que según la id del contacto de la base
+	 * de datos, devuelva su número de telefono, la comprobación
+	 * de si dichos datos apuntan a null, es para el método del 
+	 * controlador referente al error 404 
+	 * */
 	public String devolverTelefono(String id) {
 		Persona datos = bbdd.get(id);
+		if (datos == null) {
+			return null;
+		}
+		
 		String numero = datos.getTelefon();
 		return numero;
-		
 	}
-	
-	public Persona devolverPersona(String id) {
-		Persona datos = bbdd.get(id);
-		return datos;
-	}
-	
+
+	/* Añadido método que devuelve el nombre del contacto
+	 * según la id que se le pase, la comprobación de si
+	 * los datos son null, es para el método de controlador
+	 * referente al error 404
+	 * */
 	public String devolverNombre(String id) {
 		Persona datos = bbdd.get(id);
+		if (datos == null) {
+			return null;
+		}
+		
 		String nombre = datos.getNom();
 		return nombre;
 	}
